4e4e1ef7a7ff2b19954d60255c1d79ac4a31e7d0
Snehal Tripathi <snehalprdc2003@gmail.com>
2023-11-16 23:04:03 +0530

    Blog list

---
+++ src/BlogList.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const BlogList = ({ blogs, title, handleDelete }) => {
+
+    return (
+        <div className="blog-list">
+            <h1>{title}</h1>
+            {
+                blogs.map((blog) => (
+                    <div className="blog-preview" key={blog.id}>
+                        <Link to={`/blogs/${blog.id}`}>
+                            <h2>{blog.title}</h2>
+                            <p> Written By {blog.author}</p>
+                        </Link>
+
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default BlogList;
+
