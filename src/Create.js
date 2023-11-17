8c4a699ad97485a3156b1248545ae8d54fc55312
Snehal Tripathi <snehalprdc2003@gmail.com>
2023-11-17 18:57:11 +0530

    Form Created

---
+++ src/Create.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIspending] = useState(false)
+    const history = useHistory()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIspending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIspending(false);
+            history.push('/')
+        })
+    }
+
+
+    return (
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
+                {/* <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p> */}
+
+            </form>
+        </div>
+    );
+}
+
+export default Create
